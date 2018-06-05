d1040df4615138e8a065fb93c47e3ca6869fbd9a
astranomaly <xenrin@outlook.com>
2018-06-05 15:08:03 -0600

    fixed some formatting

--- plextended.user.js
+++ plextended.user.js
@@ -18,13 +18,13 @@ console.log('[plxd] ping');
 var PLXD = {};
 
 function clog(inp) {
-  return console.log("[plxd] "+inp);
+    return console.log("[plxd] "+inp);
 }
 function padder(num){
-  if(num < 10){
-    num = '0'+num;
-  }
-  return num;
+    if(num < 10){
+        num = '0'+num;
+    }
+    return num;
 }
 
 // Doesn't run except when page is refreshed. Force-run on change in navigation
@@ -33,38 +33,38 @@ function padder(num){
 // No logic for choosing total/played time (only works on played currently)
 
 PLXD = {
-  VERSION: GM_info.script.version,
-  pagePath: window.location.pathname,
-  run: function() {
-    clog("Welcome to Plextended v" + this.VERSION + "!");
+    VERSION: GM_info.script.version,
+    pagePath: window.location.pathname,
+    run: function() {
+        clog("Welcome to Plextended v" + this.VERSION + "!");
 
-    setTimeout(function(){
-      clog('Getting current vid length...');
-      let curDate = new Date();
-      let hour   = curDate.getHours();
-      let mins    = curDate.getMinutes();
-      let vidLength = document.querySelector('span[class^=PrePlayTertiaryTitleSpacer] span:not([class^=PrePlayDashSeparator]):last-of-type').innerText;
+        setTimeout(function(){
+            clog('Getting current vid length...');
+            let curDate = new Date();
+            let hour   = curDate.getHours();
+            let mins    = curDate.getMinutes();
+            let vidLength = document.querySelector('span[class^=PrePlayTertiaryTitleSpacer] span:not([class^=PrePlayDashSeparator]):last-of-type').innerText;
 
-      vidLength = Number(vidLength.match(/\d+/)[0]);
+            vidLength = Number(vidLength.match(/\d+/)[0]);
 
-      hour = Math.abs(hour - 12);
+            hour = Math.abs(hour - 12);
 
-      let upDate = mins + vidLength;
-      if( upDate >= 60 ){
-        upDate = upDate - 60;
-        hour += 1;
-        if (hour == 13){ hour = 1; }
-      }
+            let upDate = mins + vidLength;
+            if( upDate >= 60 ){
+                upDate = upDate - 60;
+                hour += 1;
+                if (hour == 13){ hour = 1; }
+            }
 
-      let target   = document.querySelector('span[class^=PrePlayTertiaryTitleSpacer]');
+            let target   = document.querySelector('span[class^=PrePlayTertiaryTitleSpacer]');
 
-      let phraseSpot = document.createElement('span');
-      phraseSpot.id = 'phraseSpot'
-      target.appendChild(phraseSpot);
-      phraseSpot.innerText = " · Done at "+padder(hour)+":"+padder(upDate);
-    },5000)
+            let phraseSpot = document.createElement('span');
+            phraseSpot.id = 'phraseSpot'
+            target.appendChild(phraseSpot);
+            phraseSpot.innerHTML = '<span class="PrePlayDashSeparator-separator-1d01z">&middot;</span>Finish by '+hour+':'+padder(upDate);
+        },5000)
 
-  }
+    }
 };
 
 PLXD.run();
